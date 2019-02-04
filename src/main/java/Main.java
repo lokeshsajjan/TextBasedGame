e8450b6186b62e6d58bf21d5acbf6abb7d7c136c
lokesh <lokeshsajjan.java@gmail.com>
2019-02-04 17:34:02 +0530

    comments are added

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import com.company.cli.adapter.incoming.CliGameLauncher;
+
+/**
+ * @author lsajjan
+ *This is the main class for running
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CliGameLauncher.startGame();
+
+	}
+
+}
